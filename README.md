119f6d5c1777cc64e5c9a50f1c9e0c8481f39a65
lucaswang <lucaswang.dev@gmail.com>
2020-06-11 01:33:23 +0800

    auto add interface

--- README.md
+++ README.md
@@ -6,14 +6,28 @@
 # 安装依赖
 npm install
 
-# 启动
+# 启动mock数据接口
 npm run start
+
+# 启动新增接口服务(在另一个Tab栏)
+npm run add
 ```
 
-### 新增接口(只需2步)
-
+### 手动新增接口(只需2步)
 - 准备好json文件，放在对应的路径下。
 - 新增相应请求类型的路由。
+
+### 自动新增接口
+```
+请求类型：POST
+请求参数：
+type:get/post/put/delete...
+path:/test/ddd
+content:xxx
+```
+![image-20200611010755331](https://tva1.sinaimg.cn/large/007S8ZIlly1gfnoxdvqa9j30o108t3z5.jpg)
+
+
 
 #### GET
 ```js
@@ -45,3 +59,10 @@ router.delete('/users/userId4', async(ctx) => {
   ctx.response.body = await fs.createReadStream(staticJsonPath + '/users/userId4.json')
 })
 ```
+
+###其他
+
+如果出错了，请删除根目录下相对应的路由，重新发起请求，添加接口。
+
+检查根目录下的index.js文件。
+
--- index.js
+++ index.js
@@ -5,10 +5,11 @@ const Router = require('koa-router');
 const cors = require('@koa/cors');
 const fs = require('fs')
 const staticJsonPath = './static/api'
-
-const app = new Koa();
 const router = new Router();
-router.prefix('/api')
+const app = new Koa();
+router.prefix('/api');
+// 服务启动的端口号
+const port = '';
 
 router.get('/users/userId', async(ctx) => {
   ctx.response.type = 'json'
@@ -29,9 +30,13 @@ router.delete('/users/userId4', async(ctx) => {
   ctx.response.type = 'json'
   ctx.response.body = await fs.createReadStream(staticJsonPath + '/users/userId4.json')
 })
-
 app
   .use(cors())
   .use(router.routes())
   .use(router.allowedMethods())
-  .listen(4000)
+  .listen(port || 4000);
+console.log(`http://localhost:${port || 4000}/api/users/userId`);
+//----------------------------------------------------------------
+// 下面开始为新增的接口
+
+  
